fb8cd415d02359737db255b5d25e4994086c987f
Zazilicious <leximurphy726000@gmail.com>
2024-12-29 12:49:03 -0500

    seccond commit

---
+++ src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log('P8 syntax highlighting is now active!');
+}
+
+export function deactivate() {}
